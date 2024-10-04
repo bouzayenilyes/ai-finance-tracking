6f57d07b6c729164d735dc476e59297052e2c20c
The Vimeleon <ilyesbouzayen@hotmail.com>
2024-10-04 17:33:05 +0100

    First Commit

---
+++ app/_components/Header.jsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useUser, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+function Header() {
+  const { user, isSignedIn } = useUser();
+  return (
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      <div className="flex flex-row items-center">
+        <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
+        <span className="text-blue-800  font-bold text-xl">FinanSmart</span>
+      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3  items-center">
+          <Link href={"/dashboard"}>
+            <Button variant="outline" className="rounded-full">
+              Dashboard
+            </Button>
+          </Link>
+          <Link href={"/sign-in"}>
+            <Button className="rounded-full">Get Started</Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
---
+++ app/_components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import { ContainerScroll } from "../../components/ui/container-scroll-animation";
+
+function Hero() {
+  return (
+    <section className="bg-gray-50 flex items-center flex-col">
+      <div className="flex flex-col overflow-hidden">
+        <ContainerScroll
+          titleComponent={
+            <>
+              <h1 className="text-4xl font-semibold text-black dark:text-white">
+                Manage your Money with AI-Driven Personal <br />
+                <span className="text-4xl md:text-[6rem] text-blue-800 font-bold mt-1 leading-none">
+                  Finance Advisor
+                </span>
+              </h1>
+            </>
+          }
+        >
+          <Image
+            src={`/dashboard.png`}
+            alt="hero"
+            height={720}
+            width={1400}
+            className="mx-auto rounded-2xl object-cover h-full object-left-top"
+            draggable={false}
+          />
+        </ContainerScroll>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
